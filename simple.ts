20959358c73af5abffc8a4355790fa3932d21d20
accenturetesting19 <149995453+accenturetesting19@users.noreply.github.com>
2023-12-08 17:01:25 +0530

    Update simple.ts

--- simple.ts
+++ simple.ts
@@ -1,4 +1,14 @@
 let message: string = 'Hello, World';
 console.log(message);
 var mysql = require('mysql');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection(
+{
+  host:'localhost',
+  user: "admin",
+  database: "project",
+  password: "mypassword", // sensitive
+  multipleStatements: true
+});
 
