e08e4fec5eb18db848a13f9851ab7bf29defbc21
accenturetesting19 <amsapna19@gmail.com>
2023-12-15 16:06:20 +0530

    Testing docker and kubernetes

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Sapna" }');
+    res.send('{ "response": "Hello From Sapna from VS code" }');
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hello World test" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, It works!" }');
+    res.send('{ "response": " Great!, It works test!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
