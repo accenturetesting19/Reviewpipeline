2fc0aa823273b89083250d12808cf927990df079
mtnorg <sapna.arunachalam@mtn.com>
2024-01-22 15:59:30 +0530

    Updating security issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+app.disable("x-powered-by");
 app.get('/', function (req, res) {
   res.send('{ "response": "Hello From Sapna from VS code" }')
 })
